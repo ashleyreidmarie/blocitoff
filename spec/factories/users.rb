c0587ab3f6fe49daa72fb1589ed1e01639660f6f
Ashley <ashleyreidmarie@gmail.com>
2016-09-15 05:58:28 +0000

    Create navigations and home page jumbotron

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
+  pw = Faker::Internet.password(8)
+  
   factory :user do
-    
+    sequence(:username, 'a'){|n| "#{Faker::Name.first_name}" << n }
+    email { Faker::Internet.email }
+    password pw
+    password_confirmation pw
   end
+  
 end
