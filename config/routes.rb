56fe469af1d1ffe3233c5ea6ce2dfd6ffdaa38e1
Ashley <ashleyreidmarie@gmail.com>
2016-10-01 07:26:21 +0000

    Generate User Controller and setup show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   get 'welcome/index'
+  resources :users, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
