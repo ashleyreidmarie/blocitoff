4323ff80c40be0636d060ce0b8c75bc83e33975d
Ashley <ashleyreidmarie@gmail.com>
2016-10-02 03:20:29 +0000

    Add devise authentication to user show view

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,15 +3,26 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   let(:user) {create(:user)}
 
-  describe "GET #show" do
-    it "returns http success" do
+  describe "for guest user" do
+    it "returns http redirect" do
       get :show, id: user.id
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to(new_user_session_path)
     end
+  end
+  
+  describe "for logged in user" do
+    before { sign_in(user) }
     
-    it "assigns current user to @user" do
-      get :show, id: user.id
-      expect(assigns(:user)).to eq(user)
+    describe "GET #show" do
+      it "returns http success" do
+        get :show, id: user.id
+        expect(response).to have_http_status(:success)
+      end
+      
+      it "assigns current user to @user" do
+        get :show, id: user.id
+        expect(assigns(:user)).to eq(user)
+      end
     end
   end
 
